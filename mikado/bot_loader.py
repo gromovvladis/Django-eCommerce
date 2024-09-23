afd840676adcd04394f4feb919e49c0c9ceefeb8
gromovvladis <s.gromovvladis@gmail.com>
2024-09-23 16:46:32 +0700

    23.09-5

--- mikado/bot_loader.py
+++ mikado/bot_loader.py
@@ -5,16 +5,9 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from django.conf import settings
 from aiogram.client.bot import DefaultBotProperties
 
-import logging
-
-logging.basicConfig(
-    format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
-    level=logging.INFO,
-)
-
 bot = Bot(token=settings.TELEGRAM_BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 if settings.DEBUG:
     storage = MemoryStorage()
 else:
-    storage = RedisStorage('redis://@127.0.0.1/', '6379', db=5, pool_size=10, prefix='bot_fsm') 
+    storage = RedisStorage('redis://@127.0.0.1:6379/5', pool_size=10, prefix='bot_fsm') 
 dp = Dispatcher(storage=storage)
