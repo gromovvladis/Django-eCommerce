5db5da4bc857f3a328b87b2c6a5a63c79c3b1d30
gromovvladis <s.gromovvladis@gmail.com>
2024-10-10 18:40:28 +0700

    10,10-1

---
+++ mikado/oscar/apps/dashboard/crm/abstract_models.py
@@ -0,0 +1,91 @@
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.utils.crypto import get_random_string
+
+from oscar.apps.catalogue.reviews.utils import get_default_review_status
+from oscar.core import validators
+
+from oscar.core.compat import AUTH_USER_MODEL
+
+
+class AbstractCRMUser(models.Model):
+    """
+    A review of a order
+    """
+
+    order = models.ForeignKey(
+        "order.Order", related_name="reviews", null=True, on_delete=models.CASCADE
+    )
+
+    # Scores are between 0 and 5
+    SCORE_CHOICES = tuple([(x, x) for x in range(5, 0, -1)])
+    score = models.SmallIntegerField("Оценка", choices=SCORE_CHOICES)
+
+    body = models.TextField("Текст отзыва")
+
+    # User information.
+    user = models.ForeignKey(
+        AUTH_USER_MODEL,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+
+    # homepage = models.URLField("URL", blank=True)
+
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    is_open = models.BooleanField("Отзыв просмотрен", default=False, db_index=True)
+
+    class Meta:
+        abstract = True
+        db_table = "order_review"
+        app_label = "customer"
+        unique_together = (("order", "user"),)
+        verbose_name ="Отзыв заказа"
+        verbose_name_plural = "Отзывы заказов"
+
+    def get_absolute_url(self):
+        kwargs = {
+            "order_slug": self.order.slug,
+            "order_pk": self.order.id,
+            "pk": self.id,
+        }
+        return reverse("customer:feedbacks", kwargs=kwargs)
+
+    def __str__(self):
+        return self.body
+
+    def clean(self):
+        self.body = self.body.strip()
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        super().delete(*args, **kwargs)
+
+    @property
+    def reviewer_name(self):
+        if self.user:
+            return self.user.get_full_name()
+        
+
+
+class AbstractTerminal(models.Model):
+    pass
+
+
+class AbstractTerminal(models.Model):
+    pass
+
+
+class AbstractTerminal(models.Model):
+    pass
+
+
+
+
+
+
--- mikado/oscar/apps/dashboard/crm/apps.py
+++ mikado/oscar/apps/dashboard/crm/apps.py
@@ -5,9 +5,11 @@ from oscar.core.loading import get_class
 
 
 class CRMDashboardConfig(OscarDashboardConfig):
-    label = "crm_dashboard"
+    label = "crm"
     name = "oscar.apps.dashboard.crm"
-    verbose_name = "Панель управления - Tillypad"
+    verbose_name = "Панель управления - CRM"
+
+    namespace = "crm"
 
     default_permissions = [
         "is_staff",
@@ -22,18 +24,49 @@ class CRMDashboardConfig(OscarDashboardConfig):
 
     # pylint: disable=attribute-defined-outside-init
     def ready(self):
-        self.crm_orders_view = get_class("dashboard.crm.views", "CRMOrdersView")
-        self.crm_stop_list_view = get_class("dashboard.crm.views", "CRMStopListView")
-        self.crm_price_list_view = get_class("dashboard.crm.views", "CRMPriceListView")
-        self.crm_couriers_view = get_class("dashboard.crm.views", "CRMCouriersView")
-        self.crm_delivery_view = get_class("dashboard.crm.views", "CRMDeliveryView")
+        self.crm_orders_list_view = get_class("dashboard.crm.views", "CRMOrderListView")
+        self.crm_partners_list_view = get_class("dashboard.crm.views", "CRMPartnerListView")
+        self.crm_staffs_list_view = get_class("dashboard.crm.views", "CRMStaffListView")
+        self.crm_products_list_view = get_class("dashboard.crm.views", "CRMProductListView")
+        self.crm_receipts_list_view = get_class("dashboard.crm.views", "CRMReceiptListView")
+        self.crm_docs_list_view = get_class("dashboard.crm.views", "CRMDocListView")
+
+        self.crm_api_staff_list_view = get_class("dashboard.crm.views", "CRMStaffEndpointView")
+        self.crm_api_terminal_list_view = get_class("dashboard.crm.views", "CRMTerminalEndpointView")
+        self.crm_api_partner_list_view = get_class("dashboard.crm.views", "CRMPartnerEndpointView")
+        self.crm_api_product_list_view = get_class("dashboard.crm.views", "CRMProductEndpointView")
+        self.crm_api_receipt_list_view = get_class("dashboard.crm.views", "CRMReceiptEndpointView")
+        self.crm_api_docs_list_view = get_class("dashboard.crm.views", "CRMDocsEndpointView")
+        self.crm_api_token_view = get_class("dashboard.crm.views", "CRMTokenEndpointView")
+        self.crm_api_subscription_view = get_class("dashboard.crm.views", "CRMSubscriptionEndpointView")
+
+        self.crm_api_user_create_view = get_class("dashboard.crm.views", "CRMUserCreateEndpointView")
+        self.crm_api_user_verify_view = get_class("dashboard.crm.views", "CRMUserVerifyEndpointView")
 
     def get_urls(self):
         urls = [
-            path("", self.crm_orders_view.as_view(), name="crm-orders"),
-            path("stop-list/", self.crm_stop_list_view.as_view(), name="crm-stop-list"),
-            path("price-list/", self.crm_price_list_view.as_view(), name="crm-price-list"),
-            path("couriers/", self.crm_couriers_view.as_view(), name="crm-couriers"),
-            path("delivery/", self.crm_delivery_view.as_view(), name="crm-delivery"),
+            path("orders/", self.crm_orders_list_view.as_view(), name="crm-orders"),
+            path("partners/", self.crm_partners_list_view.as_view(), name="crm-partners"),
+            path("staffs/", self.crm_staffs_list_view.as_view(), name="crm-staffs"),
+            path("products/", self.crm_products_list_view.as_view(), name="crm-products"),
+            path("receipts/", self.crm_receipts_list_view.as_view(), name="crm-receipts"),
+            path("docs/", self.crm_docs_list_view.as_view(), name="crm-docs"),
+        
+            
+            path("api/staffs/", self.crm_api_staff_list_view.as_view(), name="crm-api-staffs"),
+            path("api/terminals/", self.crm_api_terminal_list_view.as_view(), name="crm-api-terminals"),
+            path("api/partners/", self.crm_api_partner_list_view.as_view(), name="crm-api-partners"),
+            path("api/products/", self.crm_api_product_list_view.as_view(), name="crm-api-products"),
+            path("api/receipts/", self.crm_api_receipt_list_view.as_view(), name="crm-api-receipts"),
+            path("api/docs/", self.crm_api_docs_list_view.as_view(), name="crm-api-docs"),
+
+            path("api/subscription/setup", self.crm_api_subscription_view.as_view(), name="crm-api-setup"),
+            path("api/subscription/event", self.crm_api_subscription_view.as_view(), name="crm-api-event"),
+            
+            path("api/user/token", self.crm_api_token_view.as_view(), name="crm-api-token"),
+            path("api/user/create", self.crm_api_user_create_view.as_view(), name="crm-api-user-create"),
+            path("api/user/verify", self.crm_api_user_verify_view.as_view(), name="crm-api-user-verify"),
+
+
         ]
         return self.post_process_urls(urls)
