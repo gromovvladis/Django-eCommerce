310b5fbf081f6da1bb0557cdc9a63397663e2546
gromovvladis <s.gromovvladis@gmail.com>
2024-10-29 18:58:30 +0700

    29,10-16

--- mikado/oscar/apps/telegram/bot/handlers/staff/history.py
+++ mikado/oscar/apps/telegram/bot/handlers/staff/history.py
@@ -12,16 +12,25 @@ history_router = Router()
 @history_router.message(Command('history'))
 async def orders(message: Message, state: FSMContext):
     if await check_staff_status(message, state):
-        
         telegram_id = message.from_user.id
         user_id = await get_user_by_telegram_id(telegram_id)
         masseges = await get_messages(user_id)
         msg_list = await get_telegram_messages_message(masseges)
+        
         await message.answer("История уведомлений. Показываются последние 30 уведомлений")
         limited_msg_list = msg_list[:30]
+
         for i in range(0, len(limited_msg_list), 10):
-            msg = "\n\n".join(msg_list[i:i + 10])
-            await message.answer(msg)
+            msg = "\n\n".join(limited_msg_list[i:i + 10])
+            
+            # Проверяем длину сообщения
+            while len(msg) > 4096:
+                # Если сообщение слишком длинное, разбиваем его на части
+                await message.answer(msg[:4096])  # Отправляем первые 4096 символов
+                msg = msg[4096:]  # Убираем отправленную часть
+            # Отправляем оставшуюся часть, если она есть
+            if msg:
+                await message.answer(msg)
 
 
 
