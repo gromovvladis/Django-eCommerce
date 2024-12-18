3c76f7056630caec94cef77506e1463f5b0868fe
gromovvladis <s.gromovvladis@gmail.com>
2024-12-18 20:50:04 +0700

    18.12-11

--- site/oscar/apps/order/signals.py
+++
@@ -1,9 +0,0 @@
-import django.dispatch
-
-order_placed = django.dispatch.Signal()
-
-order_status_changed = django.dispatch.Signal()
-
-order_line_status_changed = django.dispatch.Signal()
-
-send_order_to_evotor = django.dispatch.Signal()
