bde948cadd71ea4e83096ba635ab2d06aec0e47d
gromovvladis <s.gromovvladis@gmail.com>
2025-02-25 12:44:15 +0700

    25.02-3

--- site/oscar/run_celery.py
+++ site/oscar/run_celery.py
@@ -1,14 +1,17 @@
 import os
 import logging
 from decouple import config
+
 from django.conf import settings
+
 from celery.signals import setup_logging
 from celery import Celery
 
 BROKER_URL = "redis://127.0.0.1:6379"
-CELERY_BROKER_URL = BROKER_URL
 
 CELERY_LOGGER_NAME = "celery"
+
+
 @setup_logging.connect
 def setup_celery_logging(loglevel=None, **kwargs):
     """Skip default Celery logging configuration.
@@ -18,6 +21,9 @@ def setup_celery_logging(loglevel=None, **kwargs):
     """
     if loglevel:
         logging.getLogger(CELERY_LOGGER_NAME).setLevel(loglevel)
+    else:
+        logging.getLogger(CELERY_LOGGER_NAME).setLevel(logging.INFO)
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", config("DJANGO_SETTINGS_MODULE"))
 app = Celery("celery", broker=BROKER_URL, backend=BROKER_URL)
@@ -27,7 +33,6 @@ app.conf.broker_read_url = BROKER_URL
 app.conf.broker_url = BROKER_URL
 app.conf.broker_write_url = BROKER_URL
 app.conf.result_backend = BROKER_URL
-app.conf.broker_connection_retry_on_startup = True
-app.conf.timezone = 'Asia/Krasnoyarsk'
+app.conf.timezone = "Asia/Krasnoyarsk"
 
 app.autodiscover_tasks()
