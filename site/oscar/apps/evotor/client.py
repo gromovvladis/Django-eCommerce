8e54d1897af56229d6b695df5e2faec14919839e
gromovvladis <s.gromovvladis@gmail.com>
2025-03-06 14:40:59 +0700

    06.03-17

--- site/oscar/apps/evotor/client.py
+++ site/oscar/apps/evotor/client.py
@@ -603,12 +603,7 @@ class EvotorStaffClient(EvotorAPICloud):
                 if serializer.is_valid():
                     serializer.save()
                     staff_list.append(
-                        ", ".join(
-                            [
-                                staff_json.get("name", "Имя сотрудника"),
-                                staff_json.get("last_name", "Фамилия сотрудника"),
-                            ]
-                        )
+                        f"{staff_json.get("name", "Имя")}, {staff_json.get("last_name", "Фамилия")}"
                     )
                 else:
                     json_valid = False
