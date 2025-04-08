e3dfc76feca701a86b86fd25d13da9f5eb4373e8
Vladis <160144411+gromovvladis@users.noreply.github.com>
2025-04-08 11:48:16 +0700

    Delete gromobot/apps/evotor/tests directory

--- gromobot/apps/evotor/tests/factories/requests.py
+++
@@ -1,6 +0,0 @@
-auth_data = {
-    "userId": "01-000000000000001",
-    "login": "evatormikado",
-    "password": "evatormikadopassword25",
-    "customField": "Дополнительные данные о пользователе",
-}
--- gromobot/apps/evotor/tests/views.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-
-class EvotorCloudAuthTestCase(TestCase):
-    fixtures = []
-
-    def test_is_valid_site_token(self):
-        self.assertEqual(1, 1)
